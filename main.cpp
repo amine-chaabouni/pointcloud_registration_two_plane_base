0f0534e256f9077f05ac458781b5230a5a75602c
Amine Chaabouni <aminecha3@gmail.com>
2023-05-29 19:02:41 +0200

    seperate function for point cloud processing

--- main.cpp
+++ main.cpp
@@ -14,53 +14,26 @@
 double min_angle = 10.0 * M_PI / 180.0;
 double max_angle = 80.0 * M_PI / 180.0;
 
-int main(int argc, char **argv) {
-    if (argc != 4) {
-        std::cerr << "ERROR: Syntax is octreeVisu <pcd file> <resolution>" << std::endl;
-        std::cerr << "EXAMPLE: ./main file_path voxel_size planarity_score" << std::endl;
-        return -1;
-    }
-    std::string cloud_path(argv[1]);
-    double resolution = std::atof(argv[2]);
-    double planarity_score = std::atof(argv[3]);
+using Planes = std::pair<std::vector<PlaneParam>, std::vector<pcl::Indices>>;
+using Bases = std::vector<std::pair<int, int>>;
 
-    // Load the point cloud
+std::tuple<Octree::Ptr, Planes, Bases>
+preparePointCloud(const std::string &cloud_path, double resolution, double planarity_score) {
     auto cloud = loadPcd(cloud_path);
-
-
-    std::cout << "Number of points = " << cloud->size() << std::endl;
-
     // Convert the point cloud to an octree
     Octree octree = toOctree(cloud, resolution);
     auto octree_ptr = std::make_shared<Octree>(octree);
 
-#if VISUALIZE
-    visualizeOctree(cloud, octree_ptr);
-#endif
-
-    std::cout << "Number of voxels = " << octree_ptr->getLeafCount() << std::endl;
-
-    // Remove low populated voxels
+    // Remove not needed voxels
     auto nb_removed = removeVoxelsWithLessThanXPoints(octree_ptr, 10);
-    std::cout << "Number of removed voxels = " << nb_removed << std::endl;
-
     nb_removed = removeNonPlanarVoxels(octree_ptr, planarity_score);
-    std::cout << "Number of removed voxels = " << nb_removed << std::endl;
-    std::cout << "Number of voxels = " << octree_ptr->getLeafCount() << std::endl;
-    std::cout << "Number of points = " << cloud->size() << std::endl;
-#if VISUALIZE
-    visualizeOctree(cloud, octree_ptr);
-#endif
 
+    // Generate planes
     auto planes = extractPlane(octree_ptr);
     auto plane_params = planes.first;
-    std::cout << "Number of planes = " << plane_params.size() << std::endl;
 
-#if VISUALIZE
-    visualizePlanesOnCloud(cloud, planes.second);
-#endif
-
-    std::vector <std::pair<int, int>> bases;
+    // Generate bases
+    std::vector<std::pair<int, int>> bases;
     auto nb_planes = plane_params.size();
     for (int i = 0; i < nb_planes; i++) {
         for (int j = i + 1; j < nb_planes; j++) {
@@ -75,11 +48,35 @@ int main(int argc, char **argv) {
         }
     }
 
-    std::cout << "Number of bases = " << bases.size() << std::endl;
+    return std::make_tuple(octree_ptr, planes, bases);
+
+#if VISUALIZE
+    visualizeOctree(cloud, octree_ptr);
+#endif
+
+#if VISUALIZE
+    visualizePlanesOnCloud(cloud, planes.second);
+#endif
 
 #if DEBUG
     checkOctree(octree_ptr);
 #endif
+}
+
+int main(int argc, char **argv) {
+    if (argc != 4) {
+        std::cerr << "ERROR: Syntax is octreeVisu <pcd file> <resolution>" << std::endl;
+        std::cerr << "EXAMPLE: ./main file_path voxel_size planarity_score" << std::endl;
+        return -1;
+    }
+    std::string cloud_path(argv[1]);
+    double resolution = std::atof(argv[2]);
+    double planarity_score = std::atof(argv[3]);
+
+    auto first_cloud = preparePointCloud(cloud_path, resolution, planarity_score);
+    auto octree_ptr = std::get<0>(first_cloud);
+    auto planes = std::get<1>(first_cloud);
+    auto bases = std::get<2>(first_cloud);
 
 
     return 0;
